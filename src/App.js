2ef2ff0b1d1cd5c4d244b927a46d3bc45465f044
Dennis Koopman <dennis.koopman@fizor.com>
2022-10-10 09:34:04 +0200

    Implement props.

--- src/App.js
+++ src/App.js
@@ -1,56 +1,72 @@
 import * as React from "react";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-const App = () => (
-  <div>
-    <h1>My hacker stories</h1>
+  return (
+    <div>
+      <h1>My hacker stories</h1>
 
-    <Search />
+      <Search />
 
-    <hr />
+      <hr />
 
-    <List />
-  </div>
-);
+      <List mylist={stories} />
+    </div>
+  );
+};
 
-const List = () => (
+const List = (props) => (
   <ul>
-    {list.map((item) => (
-      <li key={item.objectID}>
-        <span>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span>, author(s): {item.author}</span>
-        <span>, comments: {item.num_comments}</span>
-        <span>, points: {item.points}</span>
-      </li>
+    {props.mylist.map((item) => (
+      <Item key={item.objectID} item={item} />
     ))}
+    ;
   </ul>
 );
 
-const Search = () => (
-  <div>
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
-  </div>
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>, author(s): {props.item.author}</span>
+    <span>, comments: {props.item.num_comments}</span>
+    <span>, points: {props.item.points}</span>
+  </li>
 );
+
+const Search = () => {
+  const HandleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: element)
+    console.log(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={HandleChange} />
+    </div>
+  );
+};
 
 export default App;
